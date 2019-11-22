dd061baf0331b2025a464b4d4c6651d7eec90dc1
Ryu007 <ryuta.tashiro@gmail.com>
2019-11-23 03:02:26 +0900

    Initial Commit

---
+++ ConvertRubiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConvertRubiApp
+//
+//  Created by 田代龍太 on 2019/11/23.
+//  Copyright © 2019 田代龍太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
