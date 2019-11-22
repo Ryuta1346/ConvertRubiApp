a0b5bfe76f6082eaacb87a8efa350028196939f0
Ryu007 <ryuta.tashiro@gmail.com>
2019-11-23 03:18:25 +0900

    feat: connect Label and SearchBar to ViewController

--- ConvertRubiApp/ViewController.swift
+++ ConvertRubiApp/ViewController.swift
@@ -8,8 +8,26 @@
 
 import UIKit
 
+struct WordData: Codable {
+    let app_id:String
+    let sentence: String
+    let output_type: String
+}
+
+struct RubiData: Codable {
+    let request_id: String
+    let output_type: String
+    let converted: String
+}
+
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var searchtext: UISearchBar!
+    @IBOutlet weak var inputText: UILabel!
+    @IBOutlet weak var rubiText: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
